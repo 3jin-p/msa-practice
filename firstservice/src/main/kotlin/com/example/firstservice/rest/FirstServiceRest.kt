cf703b69a91856889b01025fe07f184e9d8cf990
sejinpark <3jin.pk@gmail.com>
2022-06-07 16:14:16 +0900

    fix: yml 오류수정. Round robin 로드밸런싱 확인

--- firstservice/src/main/kotlin/com/example/firstservice/rest/FirstServiceRest.kt
+++ firstservice/src/main/kotlin/com/example/firstservice/rest/FirstServiceRest.kt
@@ -1,12 +1,16 @@
 package com.example.firstservice.rest
 
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/first-service")
-class FirstServiceRest {
+class FirstServiceRest(
+    val env: Environment
+) {
 
     @GetMapping("/welcome")
     fun welcome():String {
@@ -15,7 +19,8 @@ class FirstServiceRest {
     }
 
     @GetMapping("/check")
-    fun check() {
-        println("check")
+    fun check(request: HttpServletRequest): String {
+        println("Server port = ${request.serverPort}")
+        return "Hi, there. This is a message from First Service on PORT ${env.getProperty("server.port")}"
     }
 }
